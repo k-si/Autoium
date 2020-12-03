8edd89cffb129c22340b055961c625f3cee6d893
k-si <1453634184@qq.com>
2020-12-03 17:54:51 +0800

    新增删除文件夹接口

--- back end/src/main/java/run/autoium/controller/ApiCaseSuiteController.java
+++ back end/src/main/java/run/autoium/controller/ApiCaseSuiteController.java
@@ -26,7 +26,7 @@ public class ApiCaseSuiteController {
     private ApiCaseSuiteServiceImpl apiCaseSuiteService;
 
     /**
-     * 保存用例文件夹
+     * 保存用例文件夹、修改文件夹
      *
      * @param apiCaseSuite 用例文件夹po对象
      * @return 如果文件夹名称为空，返回失败数据；反之返回正常数据
@@ -37,6 +37,22 @@ public class ApiCaseSuiteController {
             return R.error().message("文件夹名称不能为空");
         }
         boolean flag = apiCaseSuiteService.save(apiCaseSuite);
+        if (flag) {
+            return R.ok().data("id", apiCaseSuite.getId());
+        } else {
+            return R.error();
+        }
+    }
+
+    /**
+     * 删除文件夹
+     *
+     * @param id suite id
+     * @return 成功 / 失败
+     */
+    @GetMapping("/delete/{id}")
+    public R deleteSuite(@PathVariable Long id) {
+        boolean flag = apiCaseSuiteService.removeById(id);
         if (flag) {
             return R.ok();
         } else {
