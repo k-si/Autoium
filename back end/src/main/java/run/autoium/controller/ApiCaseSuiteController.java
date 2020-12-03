b957e4d51051e3d411351a106ac61806e4baa980
k-si <1453634184@qq.com>
2020-12-03 10:09:16 +0800

    version 1.0

--- back end/src/main/java/run/autoium/controller/ApiCaseSuiteController.java
+++ back end/src/main/java/run/autoium/controller/ApiCaseSuiteController.java
@@ -1,9 +1,13 @@
 package run.autoium.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import run.autoium.common.DataCode.response.R;
+import run.autoium.entity.po.ApiCaseSuite;
+import run.autoium.service.impl.ApiCaseSuiteServiceImpl;
 
 /**
  * <p>
@@ -14,8 +18,28 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-11-28
  */
 @RestController
-@RequestMapping("/autoium/api-case-suite")
+@RequestMapping("/apiSuite")
+@CrossOrigin
 public class ApiCaseSuiteController {
 
+    @Autowired
+    private ApiCaseSuiteServiceImpl apiCaseSuiteService;
+
+    /**
+     * 保存用例文件夹
+     *
+     * @param apiCaseSuite 用例文件夹po对象
+     * @return 如果文件夹名称为空，返回失败数据；反之返回正常数据
+     */
+    @PostMapping("/save")
+    public R saveSuite(@RequestBody ApiCaseSuite apiCaseSuite) {
+        if (StringUtils.isEmpty(apiCaseSuite)) {
+            return R.error().message("添加失败");
+        }
+        apiCaseSuiteService.save(apiCaseSuite);
+        return R.ok().message("添加成功");
+    }
+
 }
+
 
