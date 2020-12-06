c72ff89994f18dd20570b11c263f60a6ec2c0295
zibizibizibihehe1117 <1468188506@qq.com>
2020-12-06 10:39:26 +0800

    front

--- front end/杜金倪/vue-admin-template-master/src/views/tree/index.vue
+++ front end/杜金倪/vue-admin-template-master/src/views/tree/index.vue
@@ -313,14 +313,7 @@
 .jsoneditor-poweredBy{
      display: none;
   }
-html,body,#app,.el-container{
-        /*设置内部填充为0，几个布局元素之间没有间距*/
-        padding: 0px;
-         /*外部间距也是如此设置*/
-        margin: 0px;
-        /*统一设置高度为100%*/
-        height: 100%;
-    }
+
 
 </style>
 
@@ -352,7 +345,7 @@ html,body,#app,.el-container{
         return data.label.indexOf(value) !== -1;
       },
       treeClick(data){
-        newChild = {key:'',value:''}
+        const newChild = {key:'',value:''}
         if(!data.buttonable){
           api.getApiInfoById(data.id).then(response => {
             console.log(response)
