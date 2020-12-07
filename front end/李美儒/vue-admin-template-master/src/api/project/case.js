33f9bc8726b3f592c8079cd8d0718a7a009251ad
limeiru0001 <2238231318@qq.com>
2020-12-07 14:07:46 +0800

    front

--- front end/李美儒/vue-admin-template-master/src/api/project/case.js
+++ front end/李美儒/vue-admin-template-master/src/api/project/case.js
@@ -7,7 +7,7 @@ export default{
   // caseQuery 条件对象
   getCaseListPage(current, limit, caseQuery) {
     return request({
-      url: `/api/manage/PageCaseCondition/${current}/${limit}`, // 接口地址
+      url: `/autoium/api-case-suite/PageCaseCondition/${current}/${limit}`, // 接口地址
       method: 'post', // 提交方式
       // caseQurey条件对象，后端使用RequsetBody获取数据
       // data表示把对象转换为json进行传递到接口里面
@@ -17,14 +17,14 @@ export default{
   // 删除用例
   deleteCaseId(id) {
     return request({
-      url: `/api/manage/delete/${id}`, // 接口地址
+      url: `/autoium/api-case-suite/${id}`, // 接口地址
       method: 'delete' // 提交方式
     })
   },
   // 添加用例
   addCase(testcase) {
     return request({
-      url: `/api/manage/addCase`, // 接口地址
+      url: `/autoium/api-case-suite/addCase`, // 接口地址
       method: 'post', // 提交方式
       data: testcase
     })
@@ -32,13 +32,13 @@ export default{
   // 根据用例id查询用例方法
   getCaseInfo(id) {
     return request({
-      url: `/api/manage/getCase/${id}`, // 接口地址
+      url: `/autoium/api-case-suite/getCase/${id}`, // 接口地址
       method: 'get' // 提交方式
     })
   },
   updateCase(testcase) {
     return request({
-      url: `/api/manage/updateCase`, // 接口地址
+      url: `/autoium/api-case-suite/updateCase`, // 接口地址
       method: 'post', // 提交方式
       data: testcase
     })
